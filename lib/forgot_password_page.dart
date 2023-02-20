f2f4802d0ab1286d4bab0b65ee08b98dad54b322
Yash Dhrangdhariya <yash.dhrangdhar@simformsolutions.com>
2023-02-20 10:46:43 +0530

    feature: forgot password page updated  - forgot password page updated

--- lib/forgot_password_page.dart
+++ lib/forgot_password_page.dart
@@ -1,1 +1,17 @@
 // forgot password page
+
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
